209965b063cddcc571f175853275e0e54290c8dc
Shinichi Takeda <s-takeda@gimmick-pj.jp>
2025-10-02 15:18:19 +0900

    新規追加

---
+++ .sfdx/typings/lwc/apex/OpportunityLineItemsController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/OpportunityLineItemsController.getOpportunityLineItems" {
+  export default function getOpportunityLineItems(param: {opportunityId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/OpportunityLineItemsController.deleteOpportunityLineItem" {
+  export default function deleteOpportunityLineItem(param: {lineItemId: any}): Promise<any>;
+}
